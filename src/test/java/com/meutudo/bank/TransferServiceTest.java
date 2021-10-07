156a238b95c401e225770c910af2c790751ed089
Ryan Fernandes <ryanfrnnds@gmail.com>
2021-10-07 15:42:36 -0300

    Estrutura para escrever lógica do Transferencia com TDD

--- src/test/java/com/meutudo/bank/TransferServiceTest.java
+++ src/test/java/com/meutudo/bank/TransferServiceTest.java
@@ -1,13 +1,17 @@
 package com.meutudo.bank;
 
+import com.meutudo.bank.service.TransferService;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 public class TransferServiceTest {
-/*
+
     @TestConfiguration
-    static class TransferServiceTestConfiguration() {
+    static class TransferServiceTestConfiguration {
 
         @Bean
         public TransferService transferService() {
@@ -17,5 +21,4 @@ public class TransferServiceTest {
 
     @Autowired
     TransferService transferService;
-*/
 }
