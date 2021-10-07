4f8f57b8f42a4ee8b6c1b0457562d65519d0fd0f
Ryan Fernandes <ryanfrnnds@gmail.com>
2021-10-07 15:40:39 -0300

    Estrutura para escrever lógica do Transferencia

--- src/main/java/com/meutudo/bank/controller/TransferController.java
+++ src/main/java/com/meutudo/bank/controller/TransferController.java
@@ -1,9 +1,13 @@
 package com.meutudo.bank.controller;
 
+import com.meutudo.bank.service.TransferService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/transfers")
 public class TransferController {
+    @Autowired
+    TransferService transferService;
 }
--- src/main/java/com/meutudo/bank/service/TransferService.java
+++ src/main/java/com/meutudo/bank/service/TransferService.java
@@ -1,7 +1,12 @@
 package com.meutudo.bank.service;
 
+import com.meutudo.bank.repository.TransferRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TransferService {
+
+    @Autowired
+    TransferRepository transferRepository;
 }
